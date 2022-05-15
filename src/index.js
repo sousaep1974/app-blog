04384fbed074c9b1a44f22c29e60a8fbe59bd347
Edivan Sousa <94314273+sousaep1974@users.noreply.github.com>
2022-05-15 01:00:55 -0300

    Teste

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 ReactDOM.render(
-  <div id="app">
-   <h1>APP-Blog</h1>
-   <p className="title">Aplicação de Mídia</p>
-   <p className="text">Aplicação de Mídia</p>
-  </div>,
-  document.getElementById("root")
+<div id="app">
+<h1>APP-Blog</h1>
+<p className="title">Título</p>
+<p className="text">Texto</p>
+</div>,
+document.getElementById("root")
 );
