734a3e9e87d2c7abb23c02ecf3c674995a7d7608
Edivan Sousa <94314273+sousaep1974@users.noreply.github.com>
2022-05-14 23:21:04 -0300

    Atualizar

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
+
+ReactDOM.render(
+  <div id="app">
+   <h1>APP-Blog</h1>
+   <p className="title">Aplicação de Mídia</p>
+   <p className="text">Aplicação de Mídia</p>
+  </div>,
+  document.getElementById("root")
+);
